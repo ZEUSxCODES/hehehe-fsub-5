896426b3b8eee6d25be4ec27b3ae7da75617a725
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-04-26 09:54:46 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    # Update the value of UPDATES_CHANNEL with your actual channel ID or username
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
+    UPDATES_CHANNEL1 = os.environ.get("UPDATES_CHANNEL1")
+    UPDATES_CHANNEL2 = os.environ.get("UPDATES_CHANNEL2")
+    UPDATES_CHANNEL3 = os.environ.get("UPDATES_CHANNEL3")
+    UPDATES_CHANNEL4 = os.environ.get("UPDATES_CHANNEL4")
